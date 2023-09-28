1ef9330763a8d1b8246c2f155baa954747617b0e
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-28 15:43:09 +0200

    finish in-e ex1

--- intermediate.js
+++ intermediate.js
@@ -1,2 +1,18 @@
 
 
+// Capitalize
+
+const capitalize = (function() {
+    let text = String(prompt("Write your text"));
+    let text1 = text.split("");
+    // console.log(text1);
+    let char = text1[0].toUpperCase();
+    // console.log(char);
+    delete text1[0];
+    // console.log(text1);
+
+    text1.unshift(char);
+
+    console.log(text1.join(""));
+}());
+
---
+++ tipps.js
@@ -0,0 +1,56 @@
+
+// let fname = prompt("Please write your name");
+
+// if (fname == "Serri" || fname == "Riola" || fname == "MJ") {
+//     console.log("Hello Admin");
+// } else {
+//     console.log("Hello User");
+// };
+
+// // && - and
+// // || - or
+
+// const fname = prompt("Please write your name");
+// let message = (fname == "Serri") ? "Hello Admin" : "Hello User";
+// console.log(message);
+
+// function calAvg(cr1, cr2, cr3, cr4, cr5, cr6) {
+//     let sum = cr1 + cr2 + cr3 + cr4 + cr5 + cr6;
+//     let avg = sum/6;
+//     return avg;
+// }
+
+// let student1 = calAvg(80, 90, 100, 90, 70, 100);
+
+// function calcAge() {
+//     let birthYear = Number(prompt("Please type your birth year"));
+//     if(isNaN(birthYear)){
+//         return "Sorry, you must pass a number"
+//     }
+//     let currentYear = new Date().getFullYear();
+//     return currentYear - birthYear;
+// }
+
+// console.log(calcAge());
+
+// let a = 12;
+
+{
+    // let a = 20; it wouldn't be display
+}
+
+{
+    // const a = 20; it wouldn't be display
+}
+
+{
+    //  a = 20; we would get 20, a without "var, let, const overwrite others!!!"
+}
+
+// console.log(a);
+
+
+
+
+
+
