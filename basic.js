2c121531035cd82e25f59416299c37c66176a1a1
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-28 11:33:14 +0200

    ex 3 finish

--- basic.js
+++ basic.js
@@ -17,31 +17,50 @@
 // const favoriteFoods = ["pizza", "hamburger", "ice cream", "chocolate"];
 // const favoriteFoods1 = [0,1,2,3];
 
-function randomFood (){
+// function randomFood (){
 
-    var food = Math.floor(Math.random()*4);
-    // console.log(food);
-    return food;
-}
+//     var food = Math.floor(Math.random()*4);
+//     // console.log(food);
+//     return food;
+// }
 
-var favoriteFoods = randomFood();
+// var favoriteFoods = randomFood();
 
-    switch (favoriteFoods) {
-        case 0:
-            title = 'pizza';
-            console.log(title);
-            break;
-        case 1:
-            title = 'hamburger';
-            console.log(title);
-            break;
-        case 2:
-            title = 'ice cream';
-            console.log(title);
-            break;
-        case 3:
-            title = 'chocolate';
-            console.log(title);
-            break;
-    }
+//     switch (favoriteFoods) {
+//         case 0:
+//             title = 'pizza';
+//             console.log(title);
+//             break;
+//         case 1:
+//             title = 'hamburger';
+//             console.log(title);
+//             break;
+//         case 2:
+//             title = 'ice cream';
+//             console.log(title);
+//             break;
+//         case 3:
+//             title = 'chocolate';
+//             console.log(title);
+//             break;
+//     }
+
+
+// ex 3 | Crystal Gazer
+
+
+// Write a function named crystalGazer() that takes four arguments: number of children, 
+// partner's name, geographic location, and job title. Once the user passes the four arguments 
+// the program should output the following message on screen:
+
+// You will be a job_title in location and married to partners_name with number_childrens.
+
+
+
+const crystalGazer = (childrens, name, location, job) => {
+    console.log(`You will be a ${job} in ${location} and married to ${name} with ${childrens}`)
+};
+
+crystalGazer(5, 'Bobur', 'Wien', 'bank worker');
+
 
