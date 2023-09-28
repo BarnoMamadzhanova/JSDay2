01b7a59422ff6ae55bf82b34bd9c650f1efced8e
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-28 20:35:48 +0200

    fixing changes in intermediate

--- intermediate.js
+++ intermediate.js
@@ -5,14 +5,9 @@
 // const capitalize = (function() {
 //     let text = String(prompt("Write your text"));
 //     let text1 = text.split("");
-//     // console.log(text1);
 //     let char = text1[0].toUpperCase();
-//     // console.log(char);
 //     delete text1[0];
-//     // console.log(text1);
-
 //     text1.unshift(char);
-
 //     console.log(text1.join(""));
 // }());
 
@@ -25,7 +20,6 @@
 //     let result = [sum, average];
 //     return result;
 // };
-
 // let sum = (calcAvgGrades(3, 4, 5)[0]);
 // let average = (calcAvgGrades(3,4,5)[1]);
 // console.log(`Sum: ${sum} \nAverage ${average}`);
