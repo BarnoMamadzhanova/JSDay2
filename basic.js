fc9e9405bf64f7f72bac179e57f45a30087d927e
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-28 10:17:43 +0200

    ex 1 finish

---
+++ basic.js
@@ -0,0 +1,14 @@
+
+
+// ex 1 | Temperature v1.0
+
+let temp = Math.floor(Math.random()*26) -5;
+console.log(temp);
+
+if(temp > 10){
+    console.log( "The weather is moderate");
+}else {
+
+} console.log("The weather is cold");
+
+
