4e297353bafb5cf9ef90b6309efb8cc72daa26c1
BarnoMamadzhanova <b@mamadzhanova.com>
2023-09-28 19:41:52 +0200

    time convert finish

--- intermediate.js
+++ intermediate.js
@@ -19,14 +19,27 @@
 // Average Grade
 
 
-const calcAvgGrades = (gradeMath, gradePhysics, gradeEnglish) => {
-    let sum = gradeMath + gradePhysics + gradeEnglish;
-    let average = (sum)/3;
-    let result = [sum, average];
-    return result;
+// const calcAvgGrades = (gradeMath, gradePhysics, gradeEnglish) => {
+//     let sum = gradeMath + gradePhysics + gradeEnglish;
+//     let average = (sum)/3;
+//     let result = [sum, average];
+//     return result;
+// };
+
+// let sum = (calcAvgGrades(3, 4, 5)[0]);
+// let average = (calcAvgGrades(3,4,5)[1]);
+// console.log(`Sum: ${sum} \nAverage ${average}`);
+
+// Time Convert
+
+
+const convertTime = () => {
+    let minutes = Number(prompt("Write minutes"));
+    let hours = minutes/60;
+    let rhours = Math.floor(hours);
+    let minutes1 = (hours - rhours)*60;
+    let rminutes = Math.round(minutes1);
+    return minutes + " minutes= " + rhours + " hour(s) and " + rminutes + " minutes(s).";
 };
 
-let sum = (calcAvgGrades(3, 4, 5)[0]);
-let average = (calcAvgGrades(3,4,5)[1]);
-console.log(`Sum: ${sum} \nAverage ${average}`);
-
+console.log(convertTime());
